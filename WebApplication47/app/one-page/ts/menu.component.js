59de3ecf12b5be1ad27f92471561890e7a02ba52
gilior <liorgish@gmail.com>
2017-01-10 19:19:51 +0200

    mix

--- WebApplication47/app/one-page/ts/menu.component.js
+++ WebApplication47/app/one-page/ts/menu.component.js
@@ -11,13 +11,42 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var MenuComponent = (function () {
     function MenuComponent() {
+        this.lastscrollY = 0;
+        this.isMenuVisible = true;
     }
     MenuComponent.prototype.goTo = function (div) {
         console.log(div.id);
         console.log(div.scrollTop);
     };
+    MenuComponent.prototype.onscroll = function () {
+        console.log(window.scrollY);
+        this.isMenuVisible = window.scrollY < this.lastscrollY;
+        this.lastscrollY = window.scrollY;
+    };
+    __decorate([
+        core_1.HostListener('window:scroll'), 
+        __metadata('design:type', Function), 
+        __metadata('design:paramtypes', []), 
+        __metadata('design:returntype', void 0)
+    ], MenuComponent.prototype, "onscroll", null);
     MenuComponent = __decorate([
-        core_1.Component({ selector: 'site-menu', moduleId: module.id, templateUrl: '../html/menu.component.html', styleUrls: ['../scss/menu.component.css'] }), 
+        core_1.Component({
+            selector: 'site-menu', moduleId: module.id, templateUrl: '../html/menu.component.html', styleUrls: ['../scss/menu.component.css'],
+            animations: [
+                core_1.trigger('dynamicMenu', [
+                    core_1.state('true', core_1.style({
+                        //opacity: 1,
+                        transform: 'translateY(0)',
+                    })),
+                    core_1.state('false', core_1.style({
+                        //opacity: 0,
+                        transform: 'translateY(-100%)',
+                    })),
+                    core_1.transition('1 => 0', core_1.animate('0.5s  ease-in')),
+                    core_1.transition('0 => 1', [core_1.style({ transform: 'translateY(-100%)' }), core_1.animate('0.3s  ease-out')]),
+                ])
+            ],
+        }), 
         __metadata('design:paramtypes', [])
     ], MenuComponent);
     return MenuComponent;
--- WebApplication47/app/one-page/ts/menu.component.ts
+++ WebApplication47/app/one-page/ts/menu.component.ts
@@ -1,10 +1,37 @@
-﻿import { Component } from '@angular/core'
+﻿import { Component, HostListener, trigger, state, transition, style, animate } from '@angular/core'
 
-@Component({ selector: 'site-menu', moduleId: module.id, templateUrl: '../html/menu.component.html', styleUrls: ['../scss/menu.component.css'] })
+@Component({
+    selector: 'site-menu', moduleId: module.id, templateUrl: '../html/menu.component.html', styleUrls: ['../scss/menu.component.css'],
+    animations: [
+        trigger('dynamicMenu', [
+            state('true', style({
+                //opacity: 1,
+                transform: 'translateY(0)',
+
+            })),
+            state('false', style({
+                //opacity: 0,
+                transform: 'translateY(-100%)',
+            })),
+            transition('1 => 0', animate('0.5s  ease-in')),
+            transition('0 => 1', [style({ transform: 'translateY(-100%)' }), animate('0.3s  ease-out')]),
+        ])
+    ],
+})
 
 export class MenuComponent {
+    lastscrollY = 0;
+
     public goTo(div: HTMLDivElement): void {
         console.log(div.id);
         console.log(div.scrollTop);
+    }
+
+    isMenuVisible: boolean = true;
+
+    @HostListener('window:scroll') onscroll() {
+        console.log(window.scrollY);
+        this.isMenuVisible = window.scrollY < this.lastscrollY;
+        this.lastscrollY = window.scrollY;
     }
 }
